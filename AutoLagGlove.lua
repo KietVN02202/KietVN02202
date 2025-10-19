f7a303869a04b1b285dcf49fc33e7a19fb5bd770
KIETVN02202 <nhinlamgidungnhinnua@gmail.com>
2025-10-19 09:00:24 +0700

    Update AutoLagGlove.lua

--- AutoLagGlove.lua
+++ AutoLagGlove.lua
@@ -1,12 +1,12 @@
 local p=game.Players.LocalPlayer
 local c=p.Character or p.CharacterAdded:Wait()
+local h=c.HumanoidRootPart
 if not c:FindFirstChild("entered") then 
-c.HumanoidRootPart.CFrame=workspace.Lobby.Teleport1.CFrame 
+h.CFrame=workspace.Lobby.Teleport1.CFrame 
 task.wait(1) 
 end
-local hrp=c.HumanoidRootPart
-hrp.CFrame=CFrame.new(7.2714,-29.3799,275.2069,-0.1607,0,0.987,0,1,0,-0.987,0,-0.1607)
+h.CFrame=CFrame.new(7.2714,-29.3799,275.2069,-0.1607,0,0.987,0,1,0,-0.987,0,-0.1607)
 task.wait(.5)
-hrp.Anchored=true
+h.Anchored=true
 task.wait(10)
 game:GetService("Players").LocalPlayer:WaitForChild("Reset"):FireServer()
